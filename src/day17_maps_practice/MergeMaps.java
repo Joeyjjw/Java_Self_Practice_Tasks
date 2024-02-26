5397f74255736cc3be5544b69fd847f5180e6a30
joeyjjw <jiwang58@gmail.com>
2024-02-26 10:43:36 -0500

    updated

---
+++ src/day17_maps_practice/MergeMaps.java
@@ -0,0 +1,38 @@
+package day17_maps_practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MergeMaps {
+    public static void main(String[] args) {
+
+        Map<Character, Integer> map1 = new HashMap<>();
+        map1.put('A', 10);
+        map1.put('B', 20);
+
+        Map<Character, Integer> map2 = new HashMap<>();
+        map2.put('B', 30);
+        map2.put('C', 40);
+
+
+            Map<Character, Integer> map3 = new HashMap<>(map1);
+            map3.putAll(map1);
+            map3.putAll(map2);
+
+
+        System.out.println(map3);
+
+    }
+}
+
+/*
+3. Create a class named MergeMaps and write a program that merge two given maps.
+If a key is present in both maps, add their corresponding values together.
+
+         Example:
+               map1 = {'A'=10, 'B'=20}
+               map2 = {'B'=30, 'C'=40}
+
+         Output:
+               {'A'=10, 'B'=50, 'C'=40}
+ */
